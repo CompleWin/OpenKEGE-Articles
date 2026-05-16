69afe2d9e16837a64ded62a97f72b06c6793de65
complewin <complewin@yandex.ru>
2026-05-16 15:50:44 +0300

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-// @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
-
-// https://astro.build/config
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
@@ -32,4 +30,9 @@ export default defineConfig({
 			},
 		},
 	],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    shikiConfig: { theme: 'github-dark' }
+  },
 });
--- src/content/blog/my-really-really-first-post.md
+++ src/content/blog/my-really-really-first-post.md
@@ -3,6 +3,7 @@ title: "All, any и примеры задач"
 category: "Python"
 pubDate: 'May 05 2026'
 description: "Разбираем встроенные функции all и any на задачах ЕГЭ"
+tags: ['python']
 ---
 
 Функции `all()` и `any()` часто встречаются в задании 17...
@@ -10,5 +11,6 @@ description: "Разбираем встроенные функции all и any 
 ```python
 nums = [x for x in range(1, 100) if all(x % d != 0 for d in range(2, x))]
 ```
-
-Формула простого числа: $p > 1$ и $\forall d \in [2, p-1]: p \mod d \neq 0$
+$$
+Формула простого числа: p > 1 и \forall d \in [2, p-1]: p \mod d \neq 0
+$$
