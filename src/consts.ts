973aac0919d3698613c22ebdc43fccb7d813d8eb
complewin <complewin@yandex.ru>
2026-05-17 12:25:02 +0300

    add favicons, useful-link article and update about page

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,10 @@
 export const SITE_TITLE = 'OpenKEGE Articles';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Статьи по ЕГЭ информатика';
 export const ARTICLES = 'Все статьи';
 export const ABOUT = 'Где я?'
+export const TELEGRAM_CHANNEL = 'https://t.me/openkege';
+export const TELEGRAM_ME = 'https://t.me/nikolayivakhnenko';
+export const GITHUB_REPO = 'https://github.com/CompleWin/OpenKEGE-Articles';
 
 export const CATEGORIES = ['Python', 'Задания', 'Экзамен', 'Всякая всячина'] as const;
 
